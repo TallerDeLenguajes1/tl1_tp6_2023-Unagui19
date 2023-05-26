3172a30504b7017278d456774330a31b2e9f977d
Unagui19 <francorr199@gmail.com>
2023-05-26 19:10:20 -0300

    Punto 2 hecho

--- practico6/Program.cs
+++ practico6/Program.cs
@@ -1,31 +1,81 @@
-﻿bool anda=false;
-int num=0, i=0;
-double aux=0;
-string? numString="";
+﻿Console.WriteLine("\t-----Bienvenido a CalculadoraV1-----\t\n");
+Console.WriteLine("Elija la operación deseada: ");
+Console.WriteLine("1-Suma");
+Console.WriteLine("2-Resta");
+Console.WriteLine("3-Multiplicación");
+Console.WriteLine("4-División\n");
 
-Console.WriteLine("Ingrese un numero entero");
-// Console.ReadLine();
-numString= Console.ReadLine();
-anda = int.TryParse(numString, out num);
+String? ingreso = "";
+bool valido;
+int opcion=0;;
+ingreso=Console.ReadLine();
+valido= int.TryParse(ingreso, out opcion);
 
-while (!anda)
+while(!valido)
 {
-    Console.WriteLine(numString+" no es un número valido");
-    Console.WriteLine("Ingrese un valor válido");
-    numString= Console.ReadLine();
-    anda = int.TryParse(numString, out num);
+    Console.WriteLine("Valor incorrecto, ingrese un valor nuevamente: ");
+    valido= int.TryParse(ingreso, out opcion);
 }
 
-i=numString.Length-1;
-do
+
+int num1=0,num2=0,resultado;
+
+Console.WriteLine("\nIngrese 2 numeros para operar: ");
+Console.WriteLine("Numero 1: ");
+ingreso=Console.ReadLine();
+valido= int.TryParse(ingreso, out num1);
+if(valido)
+{
+    Console.WriteLine("\nNumero 2: ");
+    ingreso=Console.ReadLine();    
+    valido= int.TryParse(ingreso, out num2);
+
+    if(valido)
+    {
+        if (opcion==1)
+        {
+            resultado=Suma(num1, num2);
+            Console.WriteLine(num1 + " + "+ num2 + " = "+resultado);
+        }
+        else if (opcion==2)
+        {
+        resultado=Resta(num1, num2);
+            Console.WriteLine(num1 + " - "+ num2 + " = "+resultado);
+        }
+        else if (opcion==3)
+        {
+            resultado=Multipicacion(num1, num2);
+            Console.WriteLine(num1 + " * "+ num2 + " = "+resultado);
+        }
+        else
+        {
+            resultado=Division(num1, num2);
+            Console.WriteLine(num1 + " / "+ num2 + " = "+resultado);
+        }
+    }
+}
+
+
+
+int Suma(int num1, int num2)
+{
+    return num1+num2;    
+}
+
+int Resta(int num1, int num2)
 {
     
-    aux+=(num % 10) * Math.Pow(10,i);
-    // Console.WriteLine("aux= "+aux);
-    num=num/10;
-    // Console.WriteLine("num="+num);
-    i--;
+    return num1-num2;
 }
-while(num!=0);
 
-Console.WriteLine("Numero Invertido: "+aux);
+int Multipicacion(int num1, int num2)
+{
+    
+    return num1*num2;
+}
+
+int Division(int num1, int num2)
+{
+    
+    return num1 / num2;
+}
