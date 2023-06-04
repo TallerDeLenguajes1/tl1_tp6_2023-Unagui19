227633dede7c180fcbb34f6168ae42b00cacbb4d
Unagui19 <francorr199@gmail.com>
2023-06-04 19:50:37 -0300

    Punto 4 comenzado

---
+++ practico6/Punto4/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Ingrese un texto, numeros o lo que sea: ");
+string? ingresar=Console.ReadLine();
+
+int longitud;
+longitud=ingresar.Length;
+Console.WriteLine("Longitud de la cadena:" + longitud);
+
+Console.WriteLine("\nIngrese otro texto, numeros o lo que sea: ");
+string? ingresar2=Console.ReadLine();
+Console.WriteLine(string.Concat(ingresar," ",ingresar2) + "\n");
+
+
+
